68f77faeec7f212e13ad5359519990e69da212da
Olli Laukkanen <olli.laukkanen@protonmail.com>
2025-02-23 00:09:42 +0200

    feat: adds tailwind css configuration and dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-blue-300'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
