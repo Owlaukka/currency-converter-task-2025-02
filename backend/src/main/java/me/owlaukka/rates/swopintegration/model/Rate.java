32efcd2f75bc20b63a8683ed38d96c69b5442f74
Olli Laukkanen <olli.laukkanen@protonmail.com>
2025-02-26 20:08:39 +0200

    docs: add Javadoc comment to Rate record for clarity on Swop API response

--- backend/src/main/java/me/owlaukka/rates/swopintegration/model/Rate.java
+++ backend/src/main/java/me/owlaukka/rates/swopintegration/model/Rate.java
@@ -1,5 +1,7 @@
 package me.owlaukka.rates.swopintegration.model;
 
+import me.owlaukka.rates.ExchangeRateIntegrationException;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -9,4 +11,12 @@ import java.time.LocalDate;
  * This could be generated from the GraphQL-schema.
  */
 public record Rate(String baseCurrency, String quoteCurrency, BigDecimal quote, LocalDate date) {
+    /**
+     * Not really needed, but just in case. The Schema of Swop API does specify them being non-null.
+     */
+    public Rate {
+        if (baseCurrency == null || quoteCurrency == null || quote == null || date == null) {
+            throw new ExchangeRateIntegrationException("Rate-response from Swop API is missing required fields");
+        }
+    }
 }
