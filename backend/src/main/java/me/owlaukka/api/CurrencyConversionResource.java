a8873c38ff8353e7633cdb36d2e44590ff5bf53b
Olli Laukkanen <olli.laukkanen@protonmail.com>
2025-02-26 18:26:04 +0200

    refactor: move ValidationExceptionMapper to its own file

--- backend/src/main/java/me/owlaukka/api/CurrencyConversionResource.java
+++ backend/src/main/java/me/owlaukka/api/CurrencyConversionResource.java
@@ -2,13 +2,9 @@ package me.owlaukka.api;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.validation.ConstraintViolationException;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.ext.ExceptionMapper;
-import jakarta.ws.rs.ext.Provider;
 import me.owlaukka.currencyconversion.CurrencyConversionService;
 import me.owlaukka.model.ConversionResponse;
-import me.owlaukka.model.Error;
 
 import java.math.BigDecimal;
 
@@ -31,18 +27,5 @@ public class CurrencyConversionResource implements ConversionApi {
                 .date(conversion.date());
 
         return Response.ok(response).build();
-    }
-
-    @Provider
-    public static class ValidationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
-        @Override
-        public Response toResponse(ConstraintViolationException e) {
-            Error error = new Error()
-                    .code("VALIDATION_ERROR")
-                    .message("Invalid input parameters: " + e.getMessage());
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity(error)
-                    .build();
-        }
     }
 }
---
+++ backend/src/main/java/me/owlaukka/api/exceptions/exceptionmappers/ValidationExceptionMapper.java
@@ -0,0 +1,20 @@
+package me.owlaukka.api.exceptions.exceptionmappers;
+
+import jakarta.validation.ConstraintViolationException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+import me.owlaukka.model.Error;
+
+@Provider
+public class ValidationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+    @Override
+    public Response toResponse(ConstraintViolationException e) {
+        Error error = new Error()
+                .code("VALIDATION_ERROR")
+                .message("Invalid input parameters: " + e.getMessage());
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(error)
+                .build();
+    }
+}
